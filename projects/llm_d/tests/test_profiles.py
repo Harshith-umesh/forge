3671024e8acc6ed5e8edcaedf3619220d919091d
Alberto Perdomo <aperdomo@redhat.com>
2026-08-12 10:35:48 +0100

    fix: Concurrency order in tests
    Signed-off-by: Alberto Perdomo <aperdomo@redhat.com>

--- projects/llm_d/tests/test_profiles.py
+++ projects/llm_d/tests/test_profiles.py
@@ -107,7 +107,7 @@ def test_benchmark_workloads_are_available() -> None:
         assert benchmark["timeout_seconds"] == 3600
     assert multi_turn["timeout_seconds"] == 7200
 
-    assert concurrent["args"]["rate"] == [300, 200, 100, 50, 1]
+    assert concurrent["args"]["rate"] == [1, 50, 100, 200, 300]
     assert heavy["args"]["max_seconds"] == 600
     assert "prompt_tokens_stdev=8500" in heavy["args"]["data"]
     assert "output_tokens_max=8000" in heavy["args"]["data"]
