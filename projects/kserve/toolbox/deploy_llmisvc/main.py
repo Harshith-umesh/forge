8e6571f41b5b68afb60e5a4dd71e34662c1ab08e
Kevin Pouget <kpouget@redhat.com>
2026-08-25 01:29:50 +0200

    [kserve] toolbox: deploy_llmisvc: give more time for the pods to appear

--- projects/kserve/toolbox/deploy_llmisvc/main.py
+++ projects/kserve/toolbox/deploy_llmisvc/main.py
@@ -167,7 +167,7 @@ def apply_inference_service(args, ctx):
 
 
 @on_failure(on_wait_pods_appear_failure)
-@retry(attempts=12, delay=5, backoff=1.0)
+@retry(attempts=30, delay=5, backoff=1.0)
 @task
 def wait_pods_appear(args, ctx):
     """Wait for llm-d pods to appear"""
